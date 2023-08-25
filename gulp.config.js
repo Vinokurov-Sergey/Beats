cd29417f2ff03d802f2f9975ccb8bc2a8bb96945
Vinokurov-Sergey <135457800+Vinokurov-Sergey@users.noreply.github.com>
2023-08-25 10:34:36 +0500

    5th week

---
+++ gulp.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    SRC_PATH : 'src',
+    DIST_PATH : 'dist',
+    SVG_PATH : 'dist/img',
+    STYLES_LIBS : [
+        './node_modules/normalize.css/normalize.css'
+    ],
+    JS_LIBS: [
+        './node_modules/jquery/dist/jquery.js',
+        './src/scripts/jquery.fancybox.min.js'
+    ]
+}
